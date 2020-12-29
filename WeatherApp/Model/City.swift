9cfe2923c44da02612675dfd8aa2d885ecc42835
Hani Abdallah <haniabdallah1997@yahoo.com>
2020-12-29 18:15:32 +0200

    Did design updates

--- WeatherApp/Model/City.swift
+++ WeatherApp/Model/City.swift
@@ -14,11 +14,14 @@ struct City : Codable {
     var temp : Double
     var pressure : Int
     var humidity : Int
+    var wind_speed : Double
     var weather :  [Weather]
 }
 
 struct  Weather : Codable {
     var main : String
+    var description : String
+    var icon : String
 }
 
 
--- WeatherApp/Screens/CityDetail/View/CityDetailViewController.swift
+++ WeatherApp/Screens/CityDetail/View/CityDetailViewController.swift
@@ -15,19 +15,14 @@ class CityDetailViewController: UIViewController , UITableViewDelegate , UITable
     var cityName : String = ""
     var cityLatitude : Double = 0.0
     var  cityLongitude : Double = 0.0
-  //  var resp = Response(lat: 0, lon: 0, current: 0, daily: cityDaily(pressure: 0, temp: CityDailytemperature(day: 2.0)))
-    
-    //var resp = Response(lat: 0, lon: 0, current: City(temp: 0, pressure: 0, humidity: 0, weather : We;
-    
-    var resp = Response(lat: 0, lon: 0, current: City(temp: 0, pressure: 0, humidity: 0, weather: []), daily: [])
+    var resp = Response(lat: 0, lon: 0, current: City(temp: 0, pressure: 0, humidity: 0, wind_speed: 0, weather: []), daily: [])
 
-    
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var cityNameLabel: UILabel!
-    
     @IBOutlet weak var weatherSatusLabel: UILabel!
-    
     @IBOutlet weak var CityTempLabel: UILabel!
-    
     
     @IBOutlet weak var forecastTableView: UITableView!   {
         didSet {
@@ -38,8 +33,6 @@ class CityDetailViewController: UIViewController , UITableViewDelegate , UITable
     self.forecastTableView.register(CityDetailTableViewCell.self, forCellReuseIdentifier: "CityDetailTableViewCell")
 }
 }
- 
-
     @IBAction func segmentDidChange(_ sender : UISegmentedControl){
         if sender.selectedSegmentIndex == 0 {
             
@@ -48,14 +41,10 @@ class CityDetailViewController: UIViewController , UITableViewDelegate , UITable
             guard  let urlString = URL(string: urlData) else {
                 fatalError()
             }
-            
-            
+
             viewModel.getCurrentWeatherData(externalUrl: urlString) { res in
                 
                 self.updateCityDetailView(responseResult: res)
-              
-      
-            
             }
             
         } else if sender.selectedSegmentIndex == 1 {
@@ -64,24 +53,15 @@ class CityDetailViewController: UIViewController , UITableViewDelegate , UITable
             guard  let urlString = URL(string: urlData) else {
                 fatalError()
             }
-            
-            
             viewModel.getCurrentWeatherData(externalUrl: urlString) { res in
                 
                 self.updateCityDetailView(responseResult: res)
-              
-      
-            
             }
         }
-        
     }
-   // let unit = SettingsViewController.getUnit()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
- 
-     
         let urlData = APISettings.getWeatheraApiRequestUrl(lat: cityLatitude, long: cityLongitude, unit: "metric")
         
         guard  let urlString = URL(string: urlData) else {
@@ -92,52 +72,34 @@ class CityDetailViewController: UIViewController , UITableViewDelegate , UITable
         viewModel.getCurrentWeatherData(externalUrl: urlString) { res in
             
             self.updateCityDetailView(responseResult: res)
-          
-  
-        
         }
-   
-      
     }
-
-    
-
     func updateCityDetailView(responseResult : Response){
-        
         DispatchQueue.main.async {
             self.cityNameLabel.text = self.cityName
             self.CityTempLabel.text = String( "\(Int(responseResult.current.temp))°")
-            self.weatherSatusLabel.text = String("\(responseResult.current.weather[0].main)")
+            self.weatherSatusLabel.text = String("\(responseResult.current.weather[0].description)")
+            self.humidityLabel.text = String("Humdity : \(responseResult.current.humidity)%")
+            self.windSpeedLabel.text = String("winds Speed : \(responseResult.current.wind_speed)")
+            self.weatherIcon.image = UIImage(named: "\(responseResult.current.weather[0].icon)")
+            print(responseResult.current.weather[0].icon)
             self.resp = responseResult
-
             self.forecastTableView.reloadData()
-        
-        }
-
-        }
-    
+        }}
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int ) -> Int {
- 
         return resp.daily.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         guard let CityDetailtableViewCell = tableView
             .dequeueReusableCell(withIdentifier: "CityDetailTableViewCell",
                                  for: indexPath) as? CityDetailTableViewCell
             else { fatalError() }
-
-        
        let Dailycity = resp.daily[indexPath.row]
-        
         CityDetailtableViewCell.configureCell(response:Dailycity , index: indexPath)
-      
-        
         return CityDetailtableViewCell
     }
-            
            
 }
 
